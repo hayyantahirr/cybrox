56ea9cd3230ba084babb2f524cf75269bac83faf
hayyantahirr <hayyantahir7@gmail.com>
2025-11-21 00:52:05 +0500

    Work of about page is completed

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,16 +1,16 @@
 import Hero from "@/components/about/Hero";
 import Sec1 from "@/components/about/Sec1";
 import Sec2 from "@/components/about/Sec2";
-import Sec3 from "@/components/about/Sec3";
+import Sec4 from "@/components/home/Sec4";
 import React from "react";
 
 const page = () => {
   return (
     <>
-      <Hero/>
-      <Sec1/>
-      <Sec2/>
-      <Sec3/>
+      <Hero />
+      <Sec4 />
+      <Sec1 />
+      <Sec2 />
     </>
   );
 };
--- app/page.js
+++ app/page.js
@@ -2,19 +2,21 @@ import Hero from "@/components/home/Hero";
 import Sec1 from "@/components/home/Sec1";
 import Sec2 from "@/components/home/Sec2";
 import Sec3 from "@/components/home/Sec3";
-import Sec4 from "@/components/home/Sec4";
-import Sec5 from "@/components/home/Sec5";
+import Sec6 from "@/components/about/Sec3";
+
 import React from "react";
+import Sec5 from "@/components/home/Sec5";
 
 const page = () => {
   return (
     <>
       <Hero />
-      <Sec1/>
-      <Sec2/>
-      <Sec3/>
-      <Sec4/>
-      <Sec5/>
+      <Sec1 />
+      <Sec2 />
+      <Sec3 />
+
+      <Sec6 />
+      <Sec5 />
     </>
   );
 };
