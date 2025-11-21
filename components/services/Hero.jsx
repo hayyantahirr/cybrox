68e63f99dbece364f50482505ae50ec4704ccf4c
hayyantahirr <hayyantahir7@gmail.com>
2025-11-21 22:47:43 +0500

    Hero of services have been created and added

--- components/services/Hero.jsx
+++ components/services/Hero.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import MouseEffect from "../about/MouseEffect";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative  bg-linear-to-br from-[#f4eade] to-white">
+      <MouseEffect>
+        <div className="max-w-5xl py-24 md:py-32 px-6 mx-auto text-center">
+          <h1
+            className="text-5xl md:text-6xl font-bold text-[#1b1b1b] mb-6 animate-slide-up"
+            style={{ fontFamily: "Nippo, sans-serif" }}
+          >
+            Transform Your Vision Into Reality
+          </h1>
+          <p className="text-xl md:text-2xl text-[#2a2a2a] font-light animate-fade-in">
+            Digital solutions that drive growth, inspire audiences, and elevate
+            your brand.
+          </p>
+        </div>
+      </MouseEffect>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
